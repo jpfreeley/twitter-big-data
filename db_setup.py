6bf9cb25fc6dd43f3a53c42423d47f110b7a854f
jpfreeley <jpf321@gmail.com>
2018-04-24 19:58:07 -0400

    added script to sqoop mysql table into hdfs and hive (#7)
    * tian's commit
    
    * test twitter api
    
    * delete test.py
    
    * added streaming to mysql database capability
    
    * streaming to mysql function
    
    * Update .gitignore
    
    * added script to sqoop mysql table into hive
    
    * deleted stream_apple.json

--- db_setup.py
+++ db_setup.py
@@ -4,6 +4,39 @@ import mysql.connector
 from mysql.connector import errorcode
 import config
 from dateutil import parser
+import re
+
+emoticons_str = r"""
+    (?:
+        [:=;] # Eyes
+        [oO\-]? # Nose (optional)
+        [D\)\]\(\]/\\OpP] # Mouth
+    )"""
+
+regex_str = [
+    emoticons_str,
+    r'<[^>]+>', # HTML tags
+    r'(?:@[\w_]+)', # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
+
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
+    r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
+    r'(?:[\w_]+)', # other words
+    r'(?:\S)' # anything else
+]
+
+tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
+emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
+
+def tokenize(s):
+    return tokens_re.findall(s)
+
+def preprocess(s, lowercase=False):
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    return tokens
 
 class Db():
     def __init__(self):
@@ -16,19 +49,23 @@ class Db():
         user_id = str(t["user"]["id_str"])
         screen_name = str(t['user']['screen_name'])
         created_at = parser.parse(t['created_at']).strftime('%Y-%m-%d %H:%M:%S')
+        lang = t['lang']
         text = t['text'].encode('utf8','ignore')
-        return (tweet_id, user_id, screen_name, created_at, text)
+        text = preprocess(text)
+        return (lang, tweet_id, user_id, screen_name, created_at, text)
+
 
     def store_tweet(self, json_str):
         db=mysql.connector.connect(host=config.HOST, user=config.USER, passwd=config.PASSWD, db=config.DATABASE, charset="utf8")
         cursor = db.cursor()
-        tweet_id, user_id, screen_name, created_at, text = self.parse_tweet(json_str)
-        insert_query = ('INSERT INTO %s (tweet_id, user_id, screen_name, created_at, text) VALUES ("%s", "%s", "%s", "%s", "%s")' %(config.TABLE, tweet_id, user_id, screen_name, created_at, text))
-        cursor.execute(insert_query)
-        db.commit()
-        cursor.close()
-        db.close()
-        print('SUCCESS: '+insert_query)
+        lang, tweet_id, user_id, screen_name, created_at, text = self.parse_tweet(json_str)
+        if lang == "en":
+            insert_query = ('INSERT INTO %s (tweet_id, user_id, screen_name, created_at, text) VALUES ("%s", "%s", "%s", "%s", "%s")' %(config.TABLE, tweet_id, user_id, screen_name, created_at, text))
+            cursor.execute(insert_query)
+            db.commit()
+            cursor.close()
+            db.close()
+            print('SUCCESS: '+insert_query)
         return
 
     def create_database(self, cursor):
